c7a5990a4d304a8545ec047644097c7c790272bd
Abdurahmon17 <118058641+Abdurahmon17@users.noreply.github.com>
2025-01-31 15:10:00 +0500

    Update Junior_4.py

--- Junior_4.py
+++ Junior_4.py
@@ -2,8 +2,13 @@
 #Ru: # Python: проверить, является ли число четным
 #Uz: # Python: raqam juftligini tekshiring
 
+#Junior
 number = 4
 if number % 2 == 0:
     print("Even")
 else:
     print("Odd")
+
+#Senior
+number = 4
+print("Even" if number % 2 == 0 else "Odd")
